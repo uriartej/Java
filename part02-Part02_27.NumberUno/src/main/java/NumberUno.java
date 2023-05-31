49449078c5cf412f0fab228aceb4a2dba085dd93
uriartej <99374424+uriartej@users.noreply.github.com>
2023-05-31 06:31:41 -0700

    Add files via upload

---
+++ part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        int num = numberUno();
+        System.out.println(num);
+
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+
+}
---
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,14 @@
+
+public class Word {
+
+    public static void main(String[] args) {
+
+        String name = word();
+        System.out.println(name);
+
+    }
+
+    public static String word() {
+        return "joe";
+    }
+}
