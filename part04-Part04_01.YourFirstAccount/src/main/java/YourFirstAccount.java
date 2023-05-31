eba40c13bd1a2e12798904649787e6d36006e7e8
uriartej <99374424+uriartej@users.noreply.github.com>
2023-05-31 06:36:27 -0700

    Add files via upload

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        
+        Account account = new Account("My account", 100.0);
+        account.deposit(20.0);
+        System.out.println(account);
+        
+    }
+}
